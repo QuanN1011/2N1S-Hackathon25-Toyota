40b83e68dd29658f3b2bbd9b084536c436e27cf2
Sebastian Sarinana <bestfulife746i@gmail.com>
2025-11-09 04:40:53 -0600

    Changes for creating of Pages

--- my-hackathon-app/src/components/NavBar.jsx
+++ my-hackathon-app/src/components/NavBar.jsx
@@ -1,54 +1,90 @@
 // Display navigation bar with hamburger menu, search bar, and user login
 import React, { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  //const [isMenuOpen, setIsMenuOpen] = useState(false); // never used
   // menu closed
   const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  function handleClickLogin() {
+    navigate("/login"); // this changes to the login page
+  }
+
+  function handleClickSignUp() {
+    navigate("/signup"); // this changes to the login page
+  }
+
+  function handleClickHome() {
+    navigate("/"); // this changes to the login page
+  }
+
+  function handleClickHelp() {
+    navigate("/help"); // this changes to the login page
+  }
+
+  /*function handleClickShowCar() {
+    navigate("/showcar"); // this changes to the login page
+  } */ // not used
+
+  function handleClickAllVehicles() {
+    navigate("/allvehicles"); // this changes to the login page
+  }
 
   // to toggle menu
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
   };
 
-    return (
-        <nav className = "navbar">
-            {/* Hamburger Menu */}
-            <div className = "navbar-hamburger">
-                {/* hamburger icon */}
-                <button className = "menu-button" onClick={toggleMenu}>
-                    <span className = "menu-line" />
-                    <span className = "menu-line" />
-                    <span className = "menu-line" />
-                </button>
+  return (
+    <nav className="navbar">
+      {/* Hamburger Menu */}
+      <div className="navbar-hamburger">
+        {/* hamburger icon */}
+        <button className="menu-button" onClick={toggleMenu}>
+          <span className="menu-line" />
+          <span className="menu-line" />
+          <span className="menu-line" />
+        </button>
 
-                {/* menu items */}
-                {menuOpen && (
-                    <div className = "menu-dropdown">
-                        {/* temporary menu items */}
-                        <a href = "#">Home</a>
-                        <a href = "#">All Vehicles</a>
-                        <a href = "#">Help</a>
-                    </div>
-                )}   
-            </div>
+        {/* menu items */}
+        {menuOpen && (
+          <div className="menu-dropdown">
+            {/* temporary menu items */}
+            {/*<a href="#">Home</a>*/}
+            <button onClick={handleClickHome}>Home</button>
+            <button onClick={handleClickAllVehicles}>All Vehicles</button>
+            <button onClick={handleClickHelp}>Help</button>
+          </div>
+        )}
+      </div>
 
-            {/* Search Bar */}
-            <div className = "navbar-search">
-                {/* search input */}
-                <input
-                    type="text"
-                    className = "search-input"
-                    placeholder="Search..."
-                />
-                <button className = "search-button">Search</button>
-            </div>
+      {/* Search Bar */}
+      <div className="navbar-search">
+        {/* search input */}
+        <input type="text" className="search-input" placeholder="Search..." />
+        <button className="search-button">Search</button>
+      </div>
 
-            {/* User Login */}
-            <div className = "navbar-login">
-                <button className = "login-button">Login</button>
-                <button className = "signup-button">Sign Up</button>
-            </div>
-        </nav>
-    );
-}
+      {/* User Login */}
+      <div className="navbar-login">
+        <button className="login-button" onClick={handleClickLogin}>
+          Login
+        </button>
+        <button className="signup-button" onClick={handleClickSignUp}>
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+} /*}
+
+/*
+{/* User Login }
+<div className="navbar-login">
+<button className="login-button">Login</button>
+<button className="signup-button">Sign Up</button>
+</div>
+*/
--- my-hackathon-app/src/main.jsx
+++ my-hackathon-app/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllVehicles from "./pages/AllVehicles.jsx";
+import CompareCar from "./pages/CompareCar.jsx";
+import Help from "./pages/Help.jsx";
+import ShowCar from "./pages/ShowCar.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/allvehicles" element={<AllVehicles />} />
+        <Route path="/comparecar" element={<CompareCar />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/showcar" element={<ShowCar />} />
+        {/* optional catch-all route */}
+        {/* <Route path="*" element={<div>Page not found</div>} /> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
